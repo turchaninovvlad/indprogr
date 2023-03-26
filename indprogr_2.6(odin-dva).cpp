e58c548dafd51d9e876163deb15b7cd7812b6e0f
turchaninovvlad <82947516+turchaninovvlad@users.noreply.github.com>
2023-03-26 20:13:21 +0300

    Add files via upload

---
+++ indprogr_2.6(odin-dva).cpp
@@ -0,0 +1,181 @@
+﻿#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+//C:\Users\ivc1-5\source\repos
+const double pi = 3.14159265;
+
+// класс конуса.
+class Cone {
+    double x, y, z;        // трёхмерные координаты 
+    double radius;         // радиус
+    double height;         // высота
+public:
+    // конструктор по умолчанию
+    Cone() { x = y = z = radius = height = 0; }
+    // конструктор с центром в начале координат
+    Cone(double r, double h) {
+        x = y = z = 0.0;
+        radius = r;
+        height = h;
+    }
+    // конструктор для Cone
+    Cone(double a, double b, double c, double r, double h) {
+        x = a;
+        y = b;
+        z = c;
+        radius = r;
+        height = h;
+    }
+
+    // доступ
+    void setCoordinate(double a, double b, double c) { x = a; y = b; z = c; }
+    void setRadius(double r) { radius = r; }
+    void setHeight(double h) { height = h; }
+    double getRadius() { return radius; }
+    double getHeight() { return height; }
+
+    // рлощадь поверхности
+    double area() {
+        double l = sqrt(height * height + radius * radius);
+        return pi * radius * (radius + l);
+    }
+    // объём
+    double volume() {
+        return pi * radius * radius * height / 3;
+    }
+
+    // перезагрузка ввода
+    friend ostream& operator<<(ostream& stream, Cone obj);
+};
+
+// вывод
+ostream& operator<<(ostream& stream, Cone obj) {
+    stream << "(" << obj.x << "; ";
+    stream << obj.y << "; ";
+    stream << obj.z << ") ";
+    stream << "r=" << obj.radius << " ";
+    stream << "h=" << obj.height << "\n";
+    return stream;
+}
+
+
+class halfCone : public Cone {
+    double x, y, z;        // трёхмерные координаты 
+    double radius;         // радиус
+    double halfheight;		//высота разреза считоя от основания ot osnovaniya
+    double height;         // высота
+public:
+    // конструктор по умолчанию
+    halfCone() { x = y = z = radius = height= halfheight = 0; }
+    // конструктор усечённого
+    halfCone(double a, double b, double c, double r, double h, double h1) {
+        x = a;
+        y = b;
+        z = c;
+        halfheight = h1;
+        radius = r;
+        height = h;
+    }
+    // доступ
+    void setCoordinate(double a, double b, double c) { x = a; y = b; z = c; }
+    void setRadius(double r) { radius = r; }
+    void setHeight(double h) { height = h; }
+    double getRadius() { return radius; }
+    double getHeight() { return height; }
+
+    // лощадь поверхности
+    double area() {
+        Cone a(x, y, z, radius, height);
+        double scone = a.area();
+        double rmin = ((height - halfheight) * radius) / height;
+        double minheight = height - halfheight;
+        double lmin = sqrt(minheight * minheight + rmin * rmin);//малая длинна стороны конуса
+        double spmin = pi * rmin * lmin;//малая площадь поверхности
+            double somin = pi * rmin * rmin;//малая площадь основания
+        return (scone - spmin + somin);
+    }
+    // объём
+    double volume() {
+        double minheight = height - halfheight;
+        double rmin = ((height - halfheight) * radius) / height;
+        Cone a(x, y, z, radius, height), b(x, y, z, rmin, minheight);
+        double v1, v2;
+        v1 = a.volume();
+        v2 = b.volume();
+       // cout << v1 << "  " << v2;
+        return v1 - v2;
+    }
+
+    // перезагрузка ввода
+    friend ostream& operator<<(ostream& stream, halfCone obj);
+    istream& operator>> (istream& stream)
+    {
+        stream >> x;
+        stream >>y;
+        stream >> z;
+        stream >> radius;
+        stream >> halfheight;
+        stream >> height;
+        ;
+        return stream;
+    }
+
+};
+ostream& operator<<(ostream& stream, halfCone obj) {
+    stream << "(" << obj.x << "; ";
+    stream << obj.y << "; ";
+    stream << obj.z << ") ";
+    stream << "r=" << obj.radius << " ";
+    stream << "hh=" << obj.halfheight << " ";
+    stream << "h=" << obj.height << "\n";
+    return stream;;
+}
+
+
+
+
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    Cone c1(3, 7);              // конус с центром  в начале координат радиусом  3 и высотой 7
+    Cone c2(1, 2, 3, 5, 10);    // произвольный конус
+
+    cout << "конус 1: " << c1;
+    cout << "площадь роверхности: " << c1.area() << "\n";
+    cout << "объём: " << c1.volume() << "\n";
+
+    cout << "конус 2: " << c2;
+    cout << "площадь роверхности: " << c2.area() << "\n";
+    cout << "объём: " << c2.volume() << "\n";
+    halfCone c3(1, 2, 3, 5, 10,5);
+    cout << "конус 3: " << c3;
+    cout << "площадь роверхности: " << c3.area() << "\n";
+    cout << "объём: " << c3.volume() << "\n";
+
+    halfCone c4(1, 2, 3, 2, 10, 2);
+    cout << "конус 4: " << c4;
+    cout << "площадь роверхности: " << c4.area() << "\n";
+    cout << "объём: " << c4.volume() << "\n";
+
+    Cone c5(1, 2, 3, 5, 10);
+    halfCone c5_1(1, 2, 3, 5, 10, 2);
+    halfCone c5_2(1, 2, 3, 5, 10, 4);
+    halfCone c5_3(1, 2, 3, 5, 10, 8);
+    cout << "конус 5_1: " << c5_1;
+    cout << "площадь роверхности: " << c5_1.area() << "\n";
+    cout << "объём: " << c5_1.volume() << "\n";
+    cout << "конус 5_2: " << c5_2;
+    cout << "площадь роверхности: " << c5_2.area() << "\n";
+    cout << "объём: " << c5_2.volume() << "\n";
+    cout << "конус 5_3: " << c5_3;
+    cout << "площадь роверхности: " << c5_3.area() << "\n";
+    cout << "объём: " << c5_3.volume() << "\n";
+    return 0;
+}
---
+++ indprogr_2.6(tri).cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+//C:\Users\ivc1-5\source\repos
+const double pi = 3.14159265;
+class ocean {
+public:
+    string ocean_name, ocean_location;
+    double ocean_v, ocean_h, ocean_s;
+    ocean() { ocean_name = ocean_location = ocean_v = ocean_h = ocean_s = 0; }
+    ocean(string a, string b, double c, double d, double e) {
+        ocean_name = a;
+        ocean_location = b;
+        ocean_v = c;
+        ocean_h = d;
+        ocean_s = e;
+    }
+    void printocean() {
+        cout<< "name: " << ocean_name << "\n ";
+        cout << "location: " << ocean_location << "\n ";
+        cout << "v: " << ocean_v << "\n ";
+        cout << "h: " << ocean_h << " ";
+        cout << "s: " << ocean_s << "\n";
+    }
+    istream& operator>> (istream& stream)
+    {
+        stream >> ocean_name;
+        stream >> ocean_location;
+        stream >> ocean_v;
+        stream >> ocean_h;
+        stream >> ocean_s;
+        ;
+        return stream;
+    }
+    
+    friend ostream& operator<<(ostream& stream, ocean obj);
+};
+ostream& operator<<(ostream& stream,ocean obj) {
+    stream << "name: " << obj.ocean_name << "\n ";
+    stream << "location: " << obj.ocean_location << "\n ";
+    stream << "v: " << obj.ocean_v << "\n ";
+    stream << "h: " << obj.ocean_h << " ";
+    stream << "s: " << obj.ocean_s << "\n";
+    return stream;
+}
+class sea :public ocean {
+public:
+    string sea_name, sea_location;
+    double sea_v, sea_h, sea_s;
+    sea() { sea_name = sea_location = sea_v = sea_h = sea_s = 0; }
+    sea(string a, string b, double c, double d, double e) {
+        sea_name = a;
+        sea_location = b;
+        sea_v = c;
+        sea_h = d;
+        sea_s = e;
+    }
+    istream& operator>> (istream& stream)
+    {
+        stream >> sea_name;
+        stream >> sea_location;
+        stream >> sea_v;
+        stream >> sea_h;
+        stream >> sea_s;
+        ;
+        return stream;
+    }
+    
+    friend ostream& operator<<(ostream& stream, sea obj);
+};
+ostream& operator<<(ostream& stream, sea obj) {
+    stream << "name: " << obj.sea_name << "\n ";
+    stream << "location: " << obj.sea_location << "\n ";
+    stream << "v: " << obj.sea_v << "\n ";
+    stream << "h: " << obj.sea_h << " ";
+    stream << "s: " << obj.sea_s << "\n";
+    return stream;
+}
+class zaliv : public sea {
+public:
+    string zaliv_name, zaliv_location;
+    double zaliv_v, zaliv_h, zaliv_s;
+    zaliv() { zaliv_name = zaliv_location = zaliv_v = zaliv_h = zaliv_s = 0; }
+    zaliv(string f, string g, double h, double i, double j) {
+        zaliv_name = f;
+        zaliv_location = g;
+        zaliv_v = h;
+        zaliv_h = i;
+        zaliv_s = j;
+    }
+    istream& operator>> (istream& stream)
+    {
+        stream >> zaliv_name;
+        stream >> zaliv_location;
+        stream >> zaliv_v;
+        stream >> zaliv_h;
+        stream >> zaliv_s;
+        ;
+        return stream;
+    }
+    
+    friend ostream& operator<<(ostream& stream, zaliv obj);
+
+};
+ostream& operator<<(ostream& stream, zaliv obj) {
+    stream << "name: " << obj.zaliv_name << "\n ";
+    stream << "location: " << obj.zaliv_location << "\n ";
+    stream << "v: " << obj.zaliv_v << "\n ";
+    stream << "h: " << obj.zaliv_h << " ";
+    stream << "s: " << obj.zaliv_s << "\n";
+    stream << "name: " << obj.sea_name << "\n ";
+    stream << "location: " << obj.sea_location << "\n ";
+    stream << "v: " << obj.sea_v << "\n ";
+    stream << "h: " << obj.sea_h << " ";
+    stream << "s: " << obj.sea_s << "\n";
+    return stream;
+}
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+    ocean a("a", "local", 1, 1, 1);
+    sea b("b", "local", 1, 1, 1);
+    zaliv c("c", "local", 1, 1, 1);
+    cout << a << b << c;
+    c.printocean();
+    return 0;
+}
